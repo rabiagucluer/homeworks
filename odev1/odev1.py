93c83faddda9e8decb31c40d5203e28b604a4085
rabiakoeme <rabiagucluerr@gmail.com>
2024-09-21 14:00:48 +0200

    homework_4 added

---
+++ odev1/odev1.py
@@ -0,0 +1,51 @@
+# What I do in this homework:
+
+##1##
+# create a new virtual environment with my name and install Python 3
+# conda create –n rabiaco
+# in my project the python 3 is already installed
+# with python  "conda create –n rabia python=3"
+
+##3##
+# conda env list
+# conda environments:
+#
+#base                     C:\Users\ahmet\anaconda3
+#rabia                 *  C:\Users\ahmet\anaconda3\envs\rabia
+
+
+##2##
+# conda activate rabia
+
+##4##
+# conda install numpy pandas=1.2.1
+
+##5##
+# conda list
+# numpy            1.26.4        py39h055cbcc_0
+#numpy-base        1.26.4        py39h65a83cf_0
+
+##6##
+# conda upgrade pandas
+#The following packages will be UPDATED:
+
+# pandas          1.2.1-py39hf11a4ad_0 --> 2.2.2-py39h5da7b33_0
+
+##7##
+# conda remove numpy
+#  environment location: C:\Users\ahmet\anaconda3\envs\rabia
+
+# removed specs:
+#    - numpy
+
+##8##
+#conda install seaborn matplotlib
+
+##9##
+#conda env export > environment.yaml
+
+##10##
+# conda deactivate rabia
+
+##11##
+# conda env remove –n rabia
